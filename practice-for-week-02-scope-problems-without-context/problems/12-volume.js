01cd1d29c93fac91c8ea439221c24cc7ec323cb6
emi web developer <marianseether@gmail.com>
2024-09-08 14:30:08 +0300

    ocean

--- practice-for-week-02-scope-problems-without-context/problems/12-volume.js
+++ practice-for-week-02-scope-problems-without-context/problems/12-volume.js
@@ -8,8 +8,31 @@ a height int and will return a function. The function returned by recVolume can
 then be invoked two more times with a single argument number each time (one for
 length and one for width). Once all three numbers (height, width, length) have
 been collected return the volume of the rectangle. Any subsequent calls to the 
-function returned by recVolume should continue to return the original volume.
+function returned by recVolume should continue to return the original volume.*/
 
+  
+  function recVolume(height) {
+    let dimensions = [height];
+    let volume;
+  
+    return function (dimension) {
+      if (dimensions.length < 3) {
+        dimensions.push(dimension);
+        if (dimensions.length === 3) {
+          volume = dimensions.reduce((acc, val) => acc * val, 1);
+          return volume;
+        }
+      } else {
+        return volume;
+      }
+    };
+  }
+
+
+
+
+
+/*
 Example 1:
 let table1 = recVolume(5); // returns a function
 table1(4); // returns a function
@@ -23,9 +46,7 @@ console.log(table2(1)); // prints 6
 console.log(table2(75)); // STILL prints 6
 
 ***********************************************************************/
-function recVolume(height) {
-  // Your code here
-}
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
--- practice-for-week-02-scope-problems-without-context/problems/13-dynamic-divide.js
+++ practice-for-week-02-scope-problems-without-context/problems/13-dynamic-divide.js
@@ -2,10 +2,7 @@
 Write a function named `dynamicDivide(divisor)`. The dynamicDivide function will
 return a new function that when invoked will divide the argument number by the
 divisor.
-
-
 Look below to see how this function is invoked:
-
 const halfer = dynamicDivide(2); // returns a function
 halfer(20); // returns 10
 
@@ -14,11 +11,18 @@ divideByThree(30); // returns 10
 
 const  divideByFive = dynamicDivide(5);
 divideByFive(50); // returns 10
-
-
 ***********************************************************************/
 
 function dynamicDivide(divisor) {
+return function(number) {
+  return number/divisor
+}
+
+
+
+
+
+
   // Your code here
 }
 
