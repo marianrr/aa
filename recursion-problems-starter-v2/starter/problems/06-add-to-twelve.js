d6a55ecd0ac89552dca40dbdaa96c6d1a29fbc2c
emi web developer <marianseether@gmail.com>
2024-09-17 10:17:13 +0300

    12 equals

--- recursion-problems-starter-v2/starter/problems/06-add-to-twelve.js
+++ recursion-problems-starter-v2/starter/problems/06-add-to-twelve.js
@@ -1,11 +1,21 @@
 /***********************************************************************
 Write a recursive function called `addToTwelve` that will return true if there
 are two adjacent numbers in the input array that can sum up to 12. Otherwise,
-return false.
+return false.*/
 
-Examples:
+
+const addToTwelve = (arr) => {
+if(arr.length === 1) return false
+debugger
+if(arr[0] + arr[1] === 12) return true
+if(arr.length === 2) return false
+return addToTwelve(arr.slice(1))
+}
 
 addToTwelve([1, 3, 4, 7, 5]); // true
+/*
+Examples:
+
 addToTwelve([1, 3, 4, 7, 6]); // false
 addToTwelve([1, 11, 4, 7, 6]); // true
 addToTwelve([1, 12, 4, 7, 6]); // false
