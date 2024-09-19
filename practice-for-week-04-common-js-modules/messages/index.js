119e1765d5ea2ddcaadd639a436f48fa458604f2
emi web developer <marianseether@gmail.com>
2024-09-19 19:49:17 +0300

    npm pb1

--- practice-for-week-04-common-js-modules/messages/index.js
+++ practice-for-week-04-common-js-modules/messages/index.js
@@ -1,3 +1,7 @@
 const message1 = "You are looking very spudly today!";
 const message2 = "There's a good deal on mattresses tomorrow.";
 const message3 = "Do you like to do things in unnecessarily complicated ways?";
+
+exports.msg1 = message1
+exports.msg2 = message2
+exports.msg3 = message3
