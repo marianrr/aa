46889a7e815b629a90124a503fd260b49c7873de
emi web developer <marianseether@gmail.com>
2024-09-21 17:41:41 +0300

    first phase

---
+++ practice-for-week-04-class-employee-manager-long-practice/employee.js
@@ -0,0 +1,13 @@
+class Employee {
+
+constructor(name, salary, title, manager) {
+this.name = name
+this.salary - salary
+this.title = title
+this.manager = null
+
+}
+
+}
+
+module.exports = {Employee}
