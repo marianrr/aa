b4aaac2333749a62591f659a408b46a4676942f0
emi web developer <marianseether@gmail.com>
2024-09-21 11:35:08 +0300

    class 1

--- practice-for-week-04-class-practice/classes/person.js
+++ practice-for-week-04-class-practice/classes/person.js
@@ -1,4 +1,45 @@
-// Your code here
+
+class Person {
+
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName
+    this.lastName = lastName
+    this.age = age
+  }
+
+  introduce() {
+    console.log(`Hi, I'm ${this.firstName} ${this.lastName}, and I'm ${this.age} years old.`)
+  }
+
+static introducePeople(persons) {
+if(Array.isArray(persons) == false) throw new Error("introducePeople only takes an array as an argument.")
+return persons.map(elem => {
+  if(elem instanceof Person) return elem.introduce()
+throw new Error("All items in array must be Person class instances.")
+})
+
+}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
