aa63a8a1fbba0cc15b0e08e2ce471269225ca82d
emi web developer <marianseether@gmail.com>
2024-10-03 17:42:20 +0300

    class 4

--- practice-for-week-04-class-employee-manager-long-practice/employee.js
+++ practice-for-week-04-class-employee-manager-long-practice/employee.js
@@ -4,7 +4,12 @@ class Employee {
     this.salary = salary
     this.title = title
     this.manager = manager
-  }
+    if(manager) manager.addEmployee(this)
+  
+  
+    }
+
+
 }
 
 module.exports = Employee
