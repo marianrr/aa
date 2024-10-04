04969683de31049564432ee3f3faea902f21f19a
marianrr <marianseether@gmail.com>
2024-10-04 13:28:31 +0300

    arrow func 4

--- practice-for-week-04-context-long-practice/problems/02-calculator.js
+++ practice-for-week-04-context-long-practice/problems/02-calculator.js
@@ -1,5 +1,18 @@
 // Your code here
 
+class Calculator {
+	 
+total = 0
+	
+add = num => this.total - num
+substract = num => this.total-num
+divide = num => this.total/num
+multiply = num => this.total*num
+
+}
+
+
+
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
