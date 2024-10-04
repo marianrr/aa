333b099a73d71aceca129d90c990ebf189b38623
marianrr <marianseether@gmail.com>
2024-10-04 16:41:18 +0300

    arrow func 5

--- practice-for-week-04-context-long-practice/problems/05-bind-to-an-arg.js
+++ practice-for-week-04-context-long-practice/problems/05-bind-to-an-arg.js
@@ -1,6 +1,16 @@
 function bindToAnArg(func, arg) {
-  // Your code here
+  return function() {
+    return func(arg);
+  };
 }
+
+
+
+
+
+
+
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = bindToAnArg;
--- practice-for-week-04-context-long-practice/problems/06-fancy-calculator.js
+++ practice-for-week-04-context-long-practice/problems/06-fancy-calculator.js
@@ -1,5 +1,29 @@
 // Your code here
 
+const Calculator = require("./02-calculator.js")
+
+class FancyCalculator extends Calculator {
+	total = 0
+	setTotal = newTotal => {
+		this.total = newTotal
+		return this.total
+	}
+   modulo = num => {
+	this.total = this.total%num
+	return this.total
+   }
+   squared = () => {
+	return this.total**2
+   }
+
+}
+
+
+
+
+
+
+
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
 
