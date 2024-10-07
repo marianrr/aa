9b704b41a98141ab3a7fe971f26db6fd48c69c43
marianrr <marianseether@gmail.com>
2024-10-07 16:41:13 +0300

    tdd 4

--- practice-for-week-04-tdd-style-long-practice/problems/number-fun.js
+++ practice-for-week-04-tdd-style-long-practice/problems/number-fun.js
@@ -5,6 +5,9 @@ function returnsThree() {
 
 function reciprocal(n) {
   // Your code here
+
+if(n === 0) throw new TypeError("this is an error, should not happening!")
+return n**(-1)
 }
 
 module.exports = {
--- practice-for-week-04-tdd-style-long-practice/test/number-fun-spec.js
+++ practice-for-week-04-tdd-style-long-practice/test/number-fun-spec.js
@@ -10,3 +10,21 @@ it("should return number 3", function(){
     expect(returnsThree()).to.equal(3)
 })
 })
+
+describe("function that return reciprocal number", function(){
+    it("should throw a TypeError", function(){
+        expect(() => {reciprocal(0)}).to.throw(TypeError)
+    })
+    it("should return n to power(-1)", function() {
+        expect(reciprocal(4)).to.equal(0.25)
+    })
+    it("should return n to power(-1)", function() {
+        expect(reciprocal(10)).to.equal(1/10)
+    })
+    it("should return n to power(-1)", function() {
+        expect(reciprocal(4)).to.equal(0.25)
+    })
+    it("should return n to power(-1)", function() {
+        expect(reciprocal(3/8)).to.equal(8/3)
+    })
+})
