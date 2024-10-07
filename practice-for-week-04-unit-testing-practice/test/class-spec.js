7e09d612f35be572768e33daae3a88a74a1e7d01
marianrr <marianseether@gmail.com>
2024-10-07 13:06:03 +0300

    unit-testing 4

--- practice-for-week-04-unit-testing-practice/test/class-spec.js
+++ practice-for-week-04-unit-testing-practice/test/class-spec.js
@@ -4,31 +4,53 @@ const expect = chai.expect;
 const { Word } = require("../class");
 
 describe("Word", function () {
+let word
+beforeEach(() => {
+  word = new Word("webdeveu")
+})
+
   describe("Word constructor function", function () {
     it('should have a "word" property', function () {
-      expect.fail("replace with your code");
+      //expect.fail("replace with your code");
+
+expect(word).to.exist
+
+
     });
   
     it('should set the "word" property when a new word is created', function () {
-      expect.fail("replace with your code");
+      //expect.fail("replace with your code");
+
+      expect(word).to.have.key(word)
+
+
     });
   });
 
   describe("removeVowels function", function () {
     it("should return a the word with all vowels removed", function () {
-      expect.fail("replace with your code");
+      //expect.fail("replace with your code");
+expect(word.removeVowels()).to.equal("wbdv")
+
+
     });
   });
 
   describe("removeConsonants function", function () {
     it("should return the word with the consonants removed", function () {
-      expect.fail("replace with your code");
+      //expect.fail("replace with your code");
+
+
+
     });
   });
   
   describe("pigLatin function", function () {
     it("should return the word converted to pig latin", function () {
-      expect.fail("replace with your code");
+      //expect.fail("replace with your code");
+
+
+
     });
   });
 });
