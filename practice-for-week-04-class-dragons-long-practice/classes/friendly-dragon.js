af7fd552d805119cffc44985d67899c276e504cd
marianrr <marianseether@gmail.com>
2024-10-09 22:17:05 +0300

    dragon 4

--- practice-for-week-04-class-dragons-long-practice/classes/friendly-dragon.js
+++ practice-for-week-04-class-dragons-long-practice/classes/friendly-dragon.js
@@ -6,6 +6,18 @@ super(name, color)
 this.lifeGoals = lifeGoals
 this.friend = friend
 }
+
+hasLifeGoals() {
+    console.log(`${this.name} likes to ${this.lifeGoals[0]}`)
+    console.log(`${this.name} likes to ${this.lifeGoals[1]}`)
+    console.log(`${this.name} likes to ${this.lifeGoals[2]}`)
+}
+
+helpsPeople() {
+    return `${this.name} helps their friend ${this.friend}`
+}
+
+
 }
 
 module.exports = FriendlyDragon
