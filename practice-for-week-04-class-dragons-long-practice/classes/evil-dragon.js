539c2e4dd668a18587a60105f497796729983022
marianrr <marianseether@gmail.com>
2024-10-09 22:29:55 +0300

    dragon 5

--- practice-for-week-04-class-dragons-long-practice/classes/evil-dragon.js
+++ practice-for-week-04-class-dragons-long-practice/classes/evil-dragon.js
@@ -1,1 +1,22 @@
-// Your code here
+const Dragon = require("./dragon.js")
+
+class EvilDragon extends Dragon{
+    constructor(name, color, evilDoings, nemesis) {
+super(name, color)
+this.evilDoings = evilDoings
+this.nemesis = nemesis
+    }
+
+    dontInviteThemOverForDinner() {
+        console.log(`${this.name} will ${this.evilDoings[0]}`)
+        console.log(`${this.name} will ${this.evilDoings[1]}`)
+        console.log(`${this.name} will ${this.evilDoings[2]}`)
+    }
+
+    burnsNemesis() {
+        return `${this.name} destroys ${this.nemesis} with fire! WHOOOSH!!!`
+    }
+
+}
+
+module.exports = EvilDragon
