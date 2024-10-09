fae5ab0c7aa6c811e9584afc526eb18bba3d2a90
marianrr <marianseether@gmail.com>
2024-10-09 22:08:02 +0300

    dragon 3

--- practice-for-week-04-class-dragons-long-practice/classes/dragon.js
+++ practice-for-week-04-class-dragons-long-practice/classes/dragon.js
@@ -15,6 +15,8 @@ return dragons.map(dragon => dragon.name)
   }
 }
 
+module.exports = Dragon
+
 
 
 
--- practice-for-week-04-class-dragons-long-practice/classes/friendly-dragon.js
+++ practice-for-week-04-class-dragons-long-practice/classes/friendly-dragon.js
@@ -1,1 +1,11 @@
-// Your code here
+const Dragon = require("./dragon")
+
+class FriendlyDragon extends Dragon {
+constructor(name, color, lifeGoals, friend) {
+super(name, color)
+this.lifeGoals = lifeGoals
+this.friend = friend
+}
+}
+
+module.exports = FriendlyDragon
