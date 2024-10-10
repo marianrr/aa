3e7e1f887c069d42558ea19868c609c0697062b0
emi web developer <marianseether@gmail.com>
2024-10-10 13:39:58 +0300

    tdd 8

---
+++ practice-for-week-04-tdd-style-long-practice/problems/triangle.js
@@ -0,0 +1,6 @@
+class Triangle {
+    constructor(){
+
+        
+    }
+}
---
+++ practice-for-week-04-tdd-style-long-practice/test/triangle-spec.js
@@ -0,0 +1,46 @@
+const chai = require("chai")
+const expect = chai.expect
+const Triangle = require("../problems/triangle.js")
+describe("Triangle class", function(){
+let triangle, triangle2
+
+beforeEach(function(){
+    triangle = new Triangle(10, 20, 30)
+    triangle2 = new Triangle(21, 20, 30)
+})
+
+it("should have a constructor", function(){
+    expect(triangle.side1).to.equal(10)
+    expect(triangle.side2).to.equal(20)
+    expect(triangle.side3).to.equal(30)
+})
+
+it("should return the perimeter", function(){
+    expect(triangle.getPerimeter()).to.equal(60)
+})
+
+it("should validate", function(){
+    expect(triangle.hasValidSideLengths()).to.be.false
+    expect(triangle2.hasValidSideLengths()).to.be.true
+})
+
+it("should add an isValid property to constructor", function(){
+    triangle.validate()
+    triangle2.validate()
+    expect(triangle.isValid).to.be.false
+    expect(triangle2.isValid).to.be.true
+
+})
+
+it("should return all the instances of valid triangles", function(){
+    
+})
+
+
+
+
+
+
+
+
+})
