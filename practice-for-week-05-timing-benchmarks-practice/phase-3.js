4ced64912d155f2068fd0f8a7212f76d231a235e
marianrr <marianseether@gmail.com>
2024-10-11 15:18:51 +0300

    timming 4

--- practice-for-week-05-timing-benchmarks-practice/phase-3.js
+++ practice-for-week-05-timing-benchmarks-practice/phase-3.js
@@ -1,29 +1,45 @@
 const [addNums, addManyNums] = require("./phase-1");
 
+
+
+
+
+
+
+
 function addNums10Timing(increment) {
-  // Copy your `addNums10` code here
-  // Then, add timing code
 
-  // Your code here
-
+  function addNums10(increment) {
+    let arr = []
+    for(let i = 1; i <= 10; i++) {
+      arr.push(addNums(i*increment))
+    }
+  return arr
+  }
 }
 
 
 function addManyNums10Timing(increment) {
-// Copy your `addManyNums10` code here
-// Then, add timing code
-
-  // Your code here
-
+  function addManyNums10(increment) {
+    let arr = []
+    for(let j = 1; j <= 10; j++) {
+      arr.push(addManyNums(j*increment))
+    }
+  return arr
+  }
 }
 
 
 n = 1000000
 console.log(`addNums(${n}): `);
+console.time("addNums10Timing")
 addNums10Timing(1000000);
+consoletimeEnd("addNums10Timing")
 
 console.log("\n***********\n");
 
 n = 1000
 console.log(`addManyNums(${n}): `);
+console.time("addManyNums")
 addManyNums10Timing(5000);
+console.timeEnd("addManyNums")
