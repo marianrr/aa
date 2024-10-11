fd29d1a87aefbf7ab7024d1f27e7240b062e50d6
marianrr <marianseether@gmail.com>
2024-10-11 14:53:59 +0300

    timming 3

--- practice-for-week-05-timing-benchmarks-practice/phase-2.js
+++ practice-for-week-05-timing-benchmarks-practice/phase-2.js
@@ -2,14 +2,20 @@ const [addNums, addManyNums] = require("./phase-1");
 
 // Runs `addNums` in 10 increasing increments
 function addNums10(increment) {
-  // Fill this in
-
+  let arr = []
+  for(let i = 1; i <= 10; i++) {
+    arr.push(addNums(i*increment))
+  }
+return arr
 }
 
 // Runs `addManyNums` in 10 increasing increments
 function addManyNums10(increment) {
-  // Fill this in
-
+  let arr = []
+  for(let j = 1; j <= 10; j++) {
+    arr.push(addManyNums(j*increment))
+  }
+return arr
 }
 
 module.exports = [addNums10, addManyNums10];
