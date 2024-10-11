1be260ef2dd6268f9183bde37f7df007655f4ba9
marianrr <marianseether@gmail.com>
2024-10-11 13:57:59 +0300

    timming 2

--- practice-for-week-05-timing-benchmarks-practice/phase-1.js
+++ practice-for-week-05-timing-benchmarks-practice/phase-1.js
@@ -1,14 +1,22 @@
 // Adds up positive integers from 1-n
 function addNums(n) {
-  // Fill this in
-
+  if(n <= 0) return 
+  let sum = 0
+  for(let i = 1; i <= n; i++) {
+sum += i 
+  }
+return sum
 }
 
 
 // Adds up values of addNums(1) through addNums(n)
 function addManyNums(n) {
-  // Fill this in
-
+  if(n <= 0) return
+ let sum = 0
+ for(let j = 1; j <=n; j++) {
+  sum += addNums(j)
+ }
+return sum
 }
 
 
