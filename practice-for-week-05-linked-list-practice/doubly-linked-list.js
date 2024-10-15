223c579ddd7fcc8be7179c6a9d5854df03edd614
emi web developer <marianseether@gmail.com>
2024-10-15 21:35:25 +0300

    linked list 5

--- practice-for-week-05-linked-list-practice/doubly-linked-list.js
+++ practice-for-week-05-linked-list-practice/doubly-linked-list.js
@@ -9,14 +9,53 @@ class DoublyLinkedListNode {
 class DoublyLinkedList {
   constructor() {
     // Your code here
+    this.head = null
+    this.tail = null
+    this.length = 0
+
+
+
   }
 
-  addToHead(val) {
-    // Your code here
+  addToHead(val) { 
+    
+let nd = new DoublyLinkedListNode(val)
+
+if(!this.head) {
+  this.head = nd
+  this.tail = nd
+  this.length++
+}
+else { 
+  nd.next = this.head
+  this.head.prev = nd;
+  this.head = nd
+  this.length++
+}
   }
+
+
+  
 
   addToTail(val) {
-    // Your code here
+let nd = new DoublyLinkedListNode(val)
+if(!this.head) {
+  this.head = nd
+  this.tail = nd
+  this.length++
+}
+else {
+
+
+
+
+// Link new node to current tail
+nd.prev = this.tail;
+this.tail.next = nd;
+this.tail = nd;  // Update tail to the new node
+}
+
+
   }
 
   // You can use this function to help debug
