bff11798314ec905d819a39b36ba2ab942f8063b
emi web developer <marianseether@gmail.com>
2024-10-18 21:12:52 +0300

    linked list 1

--- data-structures-linked-lists-queue-starter/implementation/02-doubly-linked-list.js
+++ data-structures-linked-lists-queue-starter/implementation/02-doubly-linked-list.js
@@ -53,26 +53,51 @@ class DoublyLinkedList {
     }
 
     removeFromHead() {
-        // Remove node at head
+        if (this.length === 0) return undefined;
 
-        // Write your hypothesis on the time complexity of this method here
+        const removedNode = this.head;
+        if (this.length === 1) {
+            this.head = this.tail = null;
+        } else {
+            this.head = this.head.next;
+            this.head.prev = null;
+        }
+
+        this.length--;
+        return removedNode.value;
     }
 
     removeFromTail() {
         // Remove node at tail
+
+        if(this.length === 0) return undefined
+
+        let ss = this.tail
+if(this.length === 1) {this.head = this.tail = null
+    
+}
+else {
+this.tail = this.tail.prev
+this.tail.next = null
+}
+this.length--
+return ss.value
 
         // Write your hypothesis on the time complexity of this method here
     }
 
     peekAtHead() {
         // Return value of head node
-
+        if(!this.length) return undefined
+return this.head.value
         // Write your hypothesis on the time complexity of this method here
     }
 
     peekAtTail() {
         // Return value of tail node
-
+        
+        if(!this.length) return undefined
+return this.tail.value
         // Write your hypothesis on the time complexity of this method here
     }
 }
--- data-structures-linked-lists-queue-starter/implementation/03-queue.js
+++ data-structures-linked-lists-queue-starter/implementation/03-queue.js
@@ -5,21 +5,44 @@ class Queue {
     constructor() {
         this.head = null;
         this.tail = null;
+        this.length = 0
     }
 
     enqueue(val) {
         // Add node to end of queue (linked list)
-
+        
+            const node = new SinglyLinkedNode(val)
+        if(!this.head) this.head = this.tail = node
+        else {
+           this.tail.next = node
+           this.tail = node
+        }
+        
+        this.length++
+        return this.length
         // Write your hypothesis on the time complexity of this method here
     }
 
     dequeue() {
         // Remove node from front of queue (linked list)
-        
+if(this.length === 0) return null
+let cc = this.head
+if(this.length === 1) {
+    this.head = this.tail = null
+}
+else {
+this.head = this.head.next
+}
+
+this.length--
+return cc.value
         // Write your hypothesis on the time complexity of this method here
     }
 
 }
+
+
+
 
 module.exports = {
     Queue,
