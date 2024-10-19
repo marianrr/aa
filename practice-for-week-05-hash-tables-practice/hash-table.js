17d30b373d36e24d627327e6be34766dc1f1335f
marianrr <marianseether@gmail.com>
2024-10-19 13:36:27 +0300

    lists 1

---
+++ practice-for-week-05-hash-tables-practice/hash-table.js
@@ -0,0 +1,44 @@
+const sha256 = require('js-sha256');
+
+class KeyValuePair {
+  constructor(key, value) {
+    this.key = key;
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class HashTable {
+
+  constructor(numBuckets = 4) {
+    // Your code here
+
+this.data = [null, null, null, null]
+
+
+  }
+
+  hash(key) {
+    // Your code here
+  }
+
+  hashMod(key) {
+    // Your code here
+  }
+
+  insertNoCollisions(key, value) {
+    // Your code here
+  }
+
+  insertWithHashCollisions(key, value) {
+    // Your code here
+  }
+
+  insert(key, value) {
+    // Your code here
+  }
+
+}
+
+
+module.exports = HashTable;
