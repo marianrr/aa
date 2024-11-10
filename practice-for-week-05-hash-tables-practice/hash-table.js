41bec83311537a907cd641dd6c6dc191e951923d
marianrr <marianseether@gmail.com>
2024-11-10 10:16:54 +0200

    hash

--- practice-for-week-05-hash-tables-practice/hash-table.js
+++ practice-for-week-05-hash-tables-practice/hash-table.js
@@ -13,15 +13,14 @@ class HashTable {
   constructor(numBuckets = 4) {
     this.capacity = numBuckets
     this.data = new Array(this.capacity).fill(null)
-
     this.count = 0
   }
 
   hash(key) {
-    const co = sha256(key).slice(0, 8)
-    let hashValue = parseInt(co, 16)
+    let str = sha256(key).slice(0, 8)
+    const so = parseInt(str, 16)
 
-    return hashValue
+    return so
   }
 
   hashMod(key) {
@@ -29,32 +28,89 @@ class HashTable {
   }
 
   insertNoCollisions(key, value) {
-    const index = this.hashMod(key);
+    const bucketIndex = this.hashMod(key);
 
-    // Check if the key already exists (assuming unique keys)
-    if (this.data[index] && this.data[index].key === key) {
-      throw new Error('hash collision or same key/value pair already exists!');
-    }
 
-    // If the slot is empty, insert the key-value pair
-    if (!this.data[index]) {
-      this.data[index] = { key, value };
+    if (!this.data[bucketIndex]) {
+      const newKeyValuePair = new KeyValuePair(key, value);
+      this.data[bucketIndex] = newKeyValuePair;
       this.count++;
     } else {
-      // If no collision handling, throw an error (as the test cases expect)
-      throw new Error('hash collision or same key/value pair already exists!');
+      if (this.data[bucketIndex].key === key && this.data[bucketIndex].value === value) {
+        throw new Error('hash collision or same key/value pair already exists!');
+      } else {
+        throw new Error('hash collision or same key/value pair already exists!');
+      }
     }
+
   }
 
   insertWithHashCollisions(key, value) {
-    // Your code here
+    const index = this.hashMod(key);
+    const pair = new KeyValuePair(key, value);
+
+    if (this.data[index] === null) {
+      this.data[index] = pair;
+      this.count++;
+    } else {
+      let current = this.data[index];
+
+      // Check if the key already exists in the linked list
+      while (current && current.key !== key) {
+        current = current.next;
+      }
+
+      if (current) {
+        // Key already exists, update the value
+        current.value = value;
+      } else {
+        // Key doesn't exist, add the new pair to the head of the linked list
+        pair.next = this.data[index];
+        this.data[index] = pair;
+        this.count++;
+      }
+    }
   }
 
   insert(key, value) {
-    // Your code here
+    if (this.data === null) {
+      throw new Error('No hash table data present');
+    }
+
+    if (this.capacity === 0) {
+      throw new Error('No capacity set for hash table');
+    }
+
+    if (key === null || value === null) {
+      throw new Error('Key or value is null');
+    }
+
+    const index = this.hashMod(key);
+    const pair = new KeyValuePair(key, value);
+
+    if (this.data[index] === null) {
+      this.data[index] = pair;
+      this.count++;
+    } else {
+      let current = this.data[index];
+
+      // Check if the key already exists in the linked list
+      while (current && current.key !== key) {
+        current = current.next;
+      }
+
+      if (current) {
+        // Key already exists, update the value
+        current.value = value;
+      } else {
+        // Key doesn't exist, add the new pair to the head of the linked list
+        pair.next = this.data[index];
+        this.data[index] = pair;
+        this.count++;
+      }
+    }
   }
 
 }
-
 
 module.exports = HashTable;
