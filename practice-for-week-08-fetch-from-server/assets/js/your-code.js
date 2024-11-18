6b8e83ecab629532207c020d87e82149b9d23e75
emi web developer <marianseether@gmail.com>
2024-11-18 07:49:09 +0200

    fetch1

--- practice-for-week-08-fetch-from-server/assets/js/your-code.js
+++ practice-for-week-08-fetch-from-server/assets/js/your-code.js
@@ -1,19 +1,58 @@
 export function getAllDogs() {
-    
+    const url = "/dogs";
+    return fetch(url);
 }
 
 export function getDogNumberTwo() {
-    
+    const url = "/dogs/2"
+    return fetch(url)
 }
 
+
 export function postNewDog() {
-    
+    const url = "/dogs"
+    const headers = { "Content-Type": "application/x-www-form-urlencoded" }
+    const body = new URLSearchParams({
+        name: "Mili",
+        age: 40
+    })
+    const options = {
+        method: "POST",
+        headers: headers,
+        body: body
+    }
+    return fetch(url, options)
 }
 
 export function postNewDogV2(name, age) {
-     
+
+    const url = "/dogs"
+    const headers = {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    const body = new URLSearchParams({
+        name: name,
+        age: age
+    })
+    const options = {
+        method: "POST",
+        headers: headers,
+        body: body
+    }
+
+    return fetch(url, options)
+
 }
 
 export function deleteDog(id) {
-      
+    const url = `/dogs/${id}/delete`
+    const options = {
+        method: "POST",
+        headers: {
+            "AUTH": "ckyut5wau0000jyv5bsrud90y"
+        }
+    }
+
+    return fetch(url, options)
+
 }
