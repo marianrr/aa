9d3914ae422c88ec03684774cc213ce63c29b44a
emi web developer <marianseether@gmail.com>
2024-11-30 14:00:16 +0200

    proiecte mai multe, doar upload, nu le-am rezolvat

--- practice-for-week-09-data-on-elements/data.js
+++ practice-for-week-09-data-on-elements/data.js
@@ -1,1 +1,17 @@
-// Your code here
+const select1 = document.getElementsByTagName("select")[0]
+const input1 = document.getElementsByTagName("input")[0]
+const ul1 = document.querySelector("ul")
+
+
+const click1 = (e) => {
+    const li1 = document.createElement("li")
+    li1.textContent = input1.value
+    li1.setAttribute("data-type", select1.value) 
+    ul1.appendChild(li1)
+    e.preventDefault()
+}
+
+
+
+
+document.getElementsByTagName("button")[0].addEventListener("click", click1)
