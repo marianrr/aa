0d593c25c7bcb736c5af689ddad3a875e68f9e4b
emi web developer <marianseether@gmail.com>
2024-12-02 17:24:22 +0200

    express1 server 2

--- practice-for-week-10-express-route-handlers/server/app.js
+++ practice-for-week-10-express-route-handlers/server/app.js
@@ -25,11 +25,58 @@ const express = require('express');
 const app = express();
 
 // Your code here
+app.use(express.json())
+app.use((req, res, next) => {
+console.log("Request body:", req.body)
+next()
+
+})
+
+app.get("/", (req, res) => {
+  res.send("... dj guga ala felipa...")
+})
+
+app.get("/artists", (req, res) => {
+  res.json(getAllArtists())
+
+})
+
+app.post("/artists", (req, res) => {
+const data = req.body
+res.status(201)
+res.json(addArtist(data))
+
+
+})
+
+app.get("/artists/latest", (req, res) => {
+
+res.json(getLatestArtist())
+})
+
+
+app.get("/artists/latest/albums", (req, res) => {
+
+res.json(getAlbumsForLatestArtist())
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
 
 // DO NOT MODIFY
 if (require.main === module) {
   const port = 8000;
-  app.listen(port, () => console.log('Server is listening on port', port));
+  app.listen(port, () => console.log('Server is listening on port', port, "http://localhost:8000"));
 } else {
   module.exports = app;
 }
