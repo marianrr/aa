0790d423eb794b7eeb44875c2b614d6b93cae5fe
emi web developer <marianseether@gmail.com>
2024-12-06 09:08:51 +0200

    react

--- practice-for-week-14-react-functional-components-jsx/src/Showcase.js
+++ practice-for-week-14-react-functional-components-jsx/src/Showcase.js
@@ -1,3 +1,6 @@
+import img from "./images/bulbasaur.jpg"
+
+
 function Showcase() {
 const favPokemon = "dj guga la felipa..."
 const pokeCharacteristics = {
@@ -7,7 +10,9 @@ const pokeCharacteristics = {
     return (
       <div>
         <h1>{favPokemon}'s Showcase Component</h1>
-        <h2>{pokeCharacteristics.type} --- {pokeCharacteristics.move}</h2>
+        <img src={img} width="300" height="300" alt="miau" style={{margin:"50px"}}/>
+        <h2>{pokeCharacteristics.type} --- {pokeCharacteristics.move}
+          <span style={{ backgroundColor:"green"}}> soto moto</span></h2>
       </div>
     );
   }
